01120f912f23a2bc36d8970681211155154f8712
sukh <bdcorps@gmail.com>
2023-01-17 14:28:21 -0500

    add to vercel json

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,10 +3,10 @@ import { NextResponse } from 'next/server'
 
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname
-  console.log(pathname)
-  if (pathname.startsWith('/services')) {
-    return NextResponse.rewrite(new URL('https://betterlegal.com/services', request.url))
-  }
+  // console.log(pathname)
+  // if (pathname.startsWith('/services')) {
+  //   return NextResponse.rewrite(new URL('https://betterlegal.com/services', request.url))
+  // }
 
   // if (pathname.startsWith("/_next")) {
   //   console.log(pathname)
